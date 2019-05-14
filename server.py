c470dac3c411fb81bbe09f212d38275fd0aa8234
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2019-05-14 15:06:44 +0200

    add questions user story

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import data_manager
 
 app = Flask(__name__)
 
@@ -6,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/list')
 def route_list():
-    return render_template('list.html')
+    questions = data_manager.get_questions()
+    return render_template('list.html', questions=questions)
 
 
 
