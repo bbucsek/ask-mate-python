37f4ffe745396c53ac01043ac06ba81c1518f163
Eszter Szluka <szluka.eszter@gmail.com>
2019-05-14 14:56:55 +0200

    variable renamed in connection

--- connection.py
+++ connection.py
@@ -5,9 +5,9 @@ def read_csv(filename):
     dict_list= []
     with open(filename, 'r') as csv_file:
         data = csv.DictReader(csv_file)
-        for story in data:
+        for row in data:
             new_dict = {}
-            for k, v in story.items():
+            for k, v in row.items():
                 new_dict[k] = v
             dict_list.append(new_dict)
     return dict_list
