49edaf7b78e751f1596a1a916d67f445f6bd75a4
Panna Kristóf <kristofpanna@gmail.com>
2019-05-15 11:17:05 +0200

    Add route_question function

--- server.py
+++ server.py
@@ -4,14 +4,17 @@ import data_manager
 app = Flask(__name__)
 
 
-
 @app.route('/list')
 def route_list():
     questions = data_manager.get_questions()
     return render_template('list.html', questions=questions)
 
 
-
+@app.route('/question/<question_id>')
+def route_question(question_id):
+    question = data_manager.get_question_by_id(question_id)
+    answers = data_manager.get_answers_by_question_id(question_id)
+    return render_template('question.html', question=question, answers=answers)
 
 
 if __name__ == '__main__':
