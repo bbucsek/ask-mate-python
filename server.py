e9f806023f375824ac47578e973f057719709cf9
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2019-05-29 20:34:37 +0200

    refactor delete comment function

--- server.py
+++ server.py
@@ -141,8 +141,7 @@ def add_comment_to_answer(answer_id):
 
 @app.route('/comments/<comment_id>/delete')
 def delete_question_comment(comment_id):
-    id = data_manager.get_question_id_by_comment_id(comment_id)
-    question_id = id['question_id']
+    question_id = data_manager.get_question_id_by_comment_id(comment_id)['question_id']
     data_manager.delete_comment_by_id(comment_id)
     return redirect(url_for('route_question', question_id=question_id))
 
